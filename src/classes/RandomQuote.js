ae3e88864c323424ba2970b2ef3304d03e9162a2
Alex <tokyopac@gmail.com>
2025-01-13 22:50:57 +0200

    Refactoring then and catch using async/await (with additional comments)

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -10,14 +10,30 @@ class RandomQuote {
     return new Quote(id, text, author);
   }
 
-  static getRandomQuoteViaAPI() {
+  /**
+   * 1. Each async function returns Promise
+   * 2. If on the "await" line of code Promise is rejected, code in the same block below "await" is not executed
+   * 3. Promise returned by the getRandomQuoteViaAPI function will be always "fulfilled"
+   * because we catch all possible errors
+   * 4. Result of the "fulfilled" promise will be either Quote or undefined
+   * 5. Therefore there is no need for try/catch block were we called this function
+   */
+  static async getRandomQuoteViaAPI() {
     const url = 'https://quoteslate.vercel.app/api/quotes/random';
     const options = {headers: {'Content-Type': 'application/json'}};
 
-    return fetch(url, options)
-      .then((response) => response.json())
-      .then(({id, quote, author}) => new Quote(id, quote, author))
-      .catch((error) => console.error(error));
+    try {
+      const response = await fetch(url, options);
+      const {id, quote, author} = await response.json();
+      // Resolves promise to Quote (promise becomes "fulfilled")
+      return new Quote(id, quote, author);
+    } catch (error) {
+      console.error(error);
+      /**
+       * 1. Returns undefined implicitly (resolves promise to undefined)
+       * 2. Promise becomes "fulfiled"
+       */
+    }
   }
 }
 
