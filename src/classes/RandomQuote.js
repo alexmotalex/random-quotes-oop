9c8c1d43f07213730ec93b8fa140b9c51508b4f4
Oleksandr <tokyopac@gmail.com>
2025-01-14 13:41:01 +0200

    Removing comments for async/await

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -10,14 +10,6 @@ class RandomQuote {
     return new Quote(id, text, author);
   }
 
-  /**
-   * 1. Each async function returns Promise
-   * 2. If on the "await" line of code Promise is rejected, code in the same block below "await" is not executed
-   * 3. Promise returned by the getRandomQuoteViaAPI function will be always "fulfilled"
-   * because we catch all possible errors
-   * 4. Result of the "fulfilled" promise will be either Quote or undefined
-   * 5. Therefore there is no need for try/catch block were we called this function
-   */
   static async getRandomQuoteViaAPI() {
     const url = 'https://quoteslate.vercel.app/api/quotes/random';
     const options = {headers: {'Content-Type': 'application/json'}};
@@ -25,14 +17,9 @@ class RandomQuote {
     try {
       const response = await fetch(url, options);
       const {id, quote, author} = await response.json();
-      // Resolves promise to Quote (promise becomes "fulfilled")
       return new Quote(id, quote, author);
     } catch (error) {
       console.error(error);
-      /**
-       * 1. Returns undefined implicitly (resolves promise to undefined)
-       * 2. Promise becomes "fulfiled"
-       */
     }
   }
 }
